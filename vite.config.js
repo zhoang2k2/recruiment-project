9f47a987ad5715bb021af33bab88c42b655fcf36
zhoang2k2 <nvhoang2012002@gmail.com>
2023-10-30 16:59:20 +0700

    fifth-update-30-10

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/recruitment-project/',
   // server: {
   //   port: 3000
   // }
